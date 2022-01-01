10d34a2f15d33b7997f7fd86b7dfaec75e610ca4
Joel Burget <joelburget@gmail.com>
2022-01-01 11:27:44 -0800

    Initial commit.

---
+++ Web_tree_sitter.ml
@@ -0,0 +1,161 @@
+type tree_cursor = Jv.t
+type node = Jv.t
+type tree = Jv.t
+type position = Jv.t
+
+let tree_sitter () = Jv.get Jv.global "TreeSitter"
+
+module Web_tree_sitter = struct
+  type t = Jv.t
+
+  let init () = Jv.call (tree_sitter ()) "init" [||] |> Fut.of_promise ~ok:ignore
+  let new' () = Jv.new' (tree_sitter ()) [||]
+end
+
+module Language = struct
+  type t = Jv.t
+
+  let load wasm =
+    Jv.call (Jv.get (tree_sitter ()) "Language") "load" [| Jv.of_string wasm |]
+    |> Fut.of_promise ~ok:(fun lang -> lang)
+  ;;
+
+  let version lang = Jv.call lang "version" [||] |> Jv.to_int
+  let field_count lang = Jv.call lang "fieldCount" [||] |> Jv.to_int
+
+  let field_id_for_name lang name =
+    Jv.call lang "fieldIdForName" [| Jv.of_string name |] |> Jv.to_int
+  ;;
+
+  let field_name_for_id lang name =
+    Jv.call lang "fieldNameForId" [| Jv.of_int name |] |> Jv.to_string
+  ;;
+
+  (* TODO: idForNodeType *)
+  let node_type_count lang = Jv.call lang "nodeTypeCount" [||] |> Jv.to_int
+  (* TODO: nodeTypeForId,nodeTypeIsNamed, nodeTypeIsVisible, query *)
+end
+
+module Parser = struct
+  type t = Jv.t
+
+  let set_language parser lang = Jv.call parser "setLanguage" [| lang |] |> ignore
+  let get_language parser = Jv.call parser "getLanguage" [||]
+  let parse parser source_code = Jv.call parser "parse" [| Jv.of_string source_code |]
+
+  let reparse parser tree source_code =
+    Jv.call parser "parse" [| Jv.of_string source_code; tree |]
+  ;;
+
+  let parse_structure parser ~f = Jv.call parser "parse" [| Jv.repr f |]
+  let reparse_structure parser tree ~f = Jv.call parser "parse" [| Jv.repr f; tree |]
+  let reset parser = Jv.call parser "reset" [||] |> ignore
+
+  let set_timeout_micros parser timeout =
+    Jv.call parser "setTimeoutMicros" [| Jv.of_int timeout |] |> ignore
+  ;;
+
+  let get_timeout_micros parser = Jv.call parser "getTimeoutMicros" [||] |> Jv.to_int
+  (* let set_logger parser cb = Jv.call parser "setLogger" [| Jv.repr cb |] *)
+  (* let get_logger parser = Jv.call parser "getLogger" [| |] *)
+end
+
+module Tree = struct
+  type t = tree
+
+  let copy tree = Jv.call tree "copy" [||]
+  let delete tree = Jv.call tree "delete" [||] |> ignore
+  let root_node tree = Jv.call tree "rootNode" [||]
+  let get_language tree = Jv.call tree "getLanguage" [||]
+  let walk tree = Jv.call tree "walk" [||]
+  (* TODO getChangedRanges *)
+end
+
+module Node = struct
+  type t = node
+
+  let type_id node = Jv.call node "typeId" [||] |> Jv.to_int
+  let type' node = Jv.call node "type" [||] |> Jv.to_string
+  let end_position node = Jv.call node "endPosition" [||]
+  let end_index node = Jv.call node "endIndex" [||] |> Jv.to_int
+  let text node = Jv.call node "text" [||] |> Jv.to_string
+  let is_named node = Jv.call node "isNamed" [||] |> Jv.to_bool
+  let has_error node = Jv.call node "hasError" [||] |> Jv.to_bool
+  let has_changes node = Jv.call node "hasChanges" [||] |> Jv.to_bool
+  let is_missing node = Jv.call node "isMissing" [||] |> Jv.to_bool
+  let ( = ) a b = Jv.call a "equals" [| b |] |> Jv.to_bool
+  let child node ix = Jv.call node "child" [| Jv.of_int ix |] |> Jv.to_option (fun x -> x)
+
+  (* let named_child node ix = Jv.call node "namedChild" [| Jv.of_string ix |] *)
+  (* let child_for_field_id node field_id = Jv.call node "childForFieldId" [| field_id |] *)
+
+  (*
+  let child_for_field_name node field_name =
+    Jv.call node "childForFieldName" [| field_name |]
+  ;;
+     *)
+
+  let to_option = Jv.to_option (fun x -> x)
+  let to_list = Jv.to_list (fun x -> x)
+  let child_count node = Jv.call node "childCount" [||] |> Jv.to_int
+  let named_child_count node = Jv.call node "namedChildCount" [||] |> Jv.to_int
+  let first_child node = Jv.call node "firstChild" [||] |> to_option
+  let first_named_child node = Jv.call node "firstNamedChild" [||] |> to_option
+  let last_child node = Jv.call node "lastChild" [||] |> to_option
+  let last_named_child node = Jv.call node "lastNamedChild" [||] |> to_option
+  let children node = Jv.call node "children" [||] |> to_list
+  let named_children node = Jv.call node "namedChildren" [||] |> to_list
+
+  (* TODO: descendantsOfType *)
+  let next_sibling node = Jv.call node "nextSibling" [||] |> to_option
+  let previous_sibling node = Jv.call node "previousSibling" [||] |> to_option
+  let next_named_sibling node = Jv.call node "nextNamedSibling" [||] |> to_option
+  let previous_named_sibling node = Jv.call node "previousNamedSibling" [||] |> to_option
+  let parent node = Jv.call node "parent" [||] |> to_option
+
+  (* TODO: descendantForIndex, namedDescendantForIndex
+   * descendantForPosition, namedDescendantForPosition
+   * *)
+  let walk node = Jv.call node "walk" [||]
+  let to_string node = Jv.call node "toString" [||] |> Jv.to_string
+end
+
+module Tree_cursor = struct
+  type t = tree_cursor
+
+  let delete cursor = Jv.call cursor "delete" [||] |> ignore
+  let reset cursor node = Jv.call cursor "reset" [| node |] |> ignore
+  let node_type cursor = Jv.call cursor "nodeType" [||] |> Jv.to_string
+  let node_type_id cursor = Jv.call cursor "nodeTypeId" [||] |> Jv.to_int
+  let node_id cursor = Jv.call cursor "nodeId" [||] |> Jv.to_int
+  let node_is_named cursor = Jv.call cursor "nodeIsNamed" [||] |> Jv.to_bool
+  let node_is_missing cursor = Jv.call cursor "nodeIsMissing" [||] |> Jv.to_bool
+  let node_text cursor = Jv.call cursor "nodeText" [||] |> Jv.to_string
+  let start_position cursor = Jv.call cursor "startPosition" [||]
+  let end_position cursor = Jv.call cursor "endPosition" [||]
+  let start_index cursor = Jv.call cursor "startIndex" [||] |> Jv.to_int
+  let end_index cursor = Jv.call cursor "endIndex" [||] |> Jv.to_int
+  let current_node cursor = Jv.call cursor "currentNode" [||]
+  let current_field_id cursor = Jv.call cursor "currentFieldId" [||] |> Jv.to_int
+  let current_field_name cursor = Jv.call cursor "currentFieldName" [||] |> Jv.to_string
+  let goto_first_child cursor = Jv.call cursor "gotoFirstChild" [||] |> Jv.to_bool
+  let goto_next_sibling cursor = Jv.call cursor "gotoNextSibling" [||] |> Jv.to_bool
+  let goto_parent cursor = Jv.call cursor "gotoParent" [||] |> Jv.to_bool
+end
+
+module Query = struct
+  type t = Jv.t
+
+  let delete query = Jv.call query "delete" [||] |> ignore
+
+  (* TODO
+  let matches query node start_pos end_pos =
+    Jv.call query "matches" [| node; start_pos; end_pos |]
+  ;;
+
+  let captures query node start_pos end_pos =
+    Jv.call query "captures" [| node; start_pos; end_pos |]
+  ;;
+  *)
+  (* TODO: predicatesForPattern, didExceedMatchLimit *)
+end
---
+++ Web_tree_sitter.mli
@@ -0,0 +1,108 @@
+type tree_cursor
+type node
+type tree
+type position
+
+module Web_tree_sitter : sig
+  type t
+
+  val init : unit -> unit Fut.or_error
+  val new' : unit -> t
+end
+
+module Language : sig
+  type t
+
+  val load : string -> t Fut.or_error
+  val version : t -> int
+  val field_count : t -> int
+  val field_id_for_name : t -> string -> int
+  val field_name_for_id : t -> int -> string
+  val node_type_count : t -> int
+end
+
+module Node : sig
+  type t = node
+
+  val type_id : t -> int
+  val type' : t -> string
+  val end_position : t -> position
+  val end_index : t -> int
+  val text : t -> string
+  val is_named : t -> bool
+  val has_error : t -> bool
+  val has_changes : t -> bool
+  val is_missing : t -> bool
+  val ( = ) : t -> t -> bool
+  val child : t -> int -> t option
+  val child_count : t -> int
+  val named_child_count : t -> int
+  val first_child : t -> t option
+  val first_named_child : t -> t option
+  val last_child : t -> t option
+  val last_named_child : t -> t option
+  val children : t -> t list
+  val named_children : t -> t list
+  val next_sibling : t -> t option
+  val previous_sibling : t -> t option
+  val next_named_sibling : t -> t option
+  val previous_named_sibling : t -> t option
+  val parent : t -> t option
+  val walk : t -> tree_cursor
+  val to_string : t -> string
+end
+
+module Tree : sig
+  type t = tree
+
+  val copy : t -> t
+  val delete : t -> unit
+  val root_node : t -> Node.t
+  val get_language : t -> Language.t
+  val walk : t -> tree_cursor
+end
+
+module Parser : sig
+  type t
+
+  val set_language : t -> Language.t -> unit
+  val get_language : t -> Language.t
+  val parse : t -> string -> Tree.t
+  val reparse : t -> Tree.t -> string -> Tree.t
+  val parse_structure : t -> f:(int -> int -> string) -> Tree.t
+  val reparse_structure : t -> Tree.t -> f:(int -> int -> string) -> Tree.t
+  val reset : t -> unit
+  val set_timeout_micros : t -> int -> unit
+  val get_timeout_micros : t -> int
+end
+
+module Tree_cursor : sig
+  type t = tree_cursor
+
+  val delete : t -> unit
+  val reset : t -> Node.t -> unit
+  val node_type : t -> string
+  val node_type_id : t -> int
+  val node_id : t -> int
+  val node_is_named : t -> bool
+  val node_is_missing : t -> bool
+  val node_text : t -> string
+  val start_position : t -> position
+  val end_position : t -> position
+  val start_index : t -> int
+  val end_index : t -> int
+  val current_node : t -> node
+  val current_field_id : t -> int
+  val current_field_name : t -> string
+  val goto_first_child : t -> bool
+  val goto_next_sibling : t -> bool
+  val goto_parent : t -> bool
+end
+
+module Query : sig
+  type t
+
+  val delete : t -> unit
+  (* TODO: val matches : t -> node -> position -> position -> ? *)
+  (* TODO: val captures : t -> node -> position -> position -> ? *)
+end
