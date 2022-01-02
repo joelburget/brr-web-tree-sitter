237b91779f409a2e1c0b59dc24025f9d95a606b6
Joel Burget <joelburget@gmail.com>
2022-01-01 19:34:37 -0800

    Add tests that update the page.

--- bin/min.ml
+++ bin/min.ml
@@ -2,7 +2,11 @@ open Brr
 open Brr_web_tree_sitter
 open Web_tree_sitter
 
-let () = El.set_children (Document.body G.document) El.[ txt' "(look in the console)" ]
+let test lbl b =
+  let result = if b then "okay" else "FAILED" in
+  El.append_children (Document.body G.document) El.[ br (); txt' (lbl ^ ": " ^ result) ]
+;;
+
 let unnamed_lang, fulfil_unnamed_lang = Fut.create ()
 let parser, fulfil_parser = Fut.create ()
 
@@ -24,7 +28,9 @@ let (_ : unit Fut.or_error) =
 let (_ : unit Fut.t) =
   let open Fut.Syntax in
   let+ lang = unnamed_lang in
-  Console.log Language.[ version lang; field_count lang; node_type_count lang ]
+  test "Language.version" (Language.version lang = 13);
+  test "Language.field_count" (Language.field_count lang = 0);
+  test "Language.node_type_count" (Language.node_type_count lang = 10)
 ;;
 
 let (_ : unit Fut.t) =
@@ -32,30 +38,45 @@ let (_ : unit Fut.t) =
   let* _ = unnamed_lang in
   let+ parser = parser in
   let tree = Parser.parse parser "x * x" in
-  Console.log [ tree |> Tree.root_node |> Node.to_string ];
+  let x = tree |> Tree.root_node |> Node.to_string in
+  test "tree |> Tree.root_node |> Node.to_string" (x = "(expr (expr) (expr))");
+  Console.log [ x ];
   (* TODO: broken *)
   let tree = Parser.reparse parser tree "x * x + y" in
-  Console.log [ tree |> Tree.root_node |> Node.to_string ];
+  let x = tree |> Tree.root_node |> Node.to_string in
+  test
+    "tree |> Tree.root_node |> Node.to_string"
+    (x = "(expr (expr (expr) (expr)) (expr))");
+  Console.log [ x ];
   let tree = Parser.parse parser "x * x + y" in
-  Console.log [ tree |> Tree.root_node |> Node.to_string ];
+  let x = tree |> Tree.root_node |> Node.to_string in
+  test
+    "tree |> Tree.root_node |> Node.to_string"
+    (x = "(expr (expr (expr) (expr)) (expr))");
+  Console.log [ x ];
   let node = Tree.root_node tree in
-  Console.log Node.[ text node; is_named node ];
+  test "Node.text" (Node.text node = "x * x + y");
+  test "Node.is_named" (Node.is_named node = true);
   (match Node.first_child node with
-  | None -> Console.log [ "first_child -> None" ]
-  | Some node' -> Console.log Node.[ node' = node; text node' ]);
+  | None -> test "Node.first_child" false
+  | Some node' ->
+    test "Node.(=)" (not Node.(node' = node));
+    test "Node.text" (Node.text node' = "x * x"));
   let cursor = Tree.walk tree in
+  test "Tree_cursor.node_type" (Tree_cursor.node_type cursor = "expr");
+  test "Tree_cursor.node_text" (Tree_cursor.node_text cursor = "x * x + y");
+  let start_pos = Tree_cursor.start_position cursor in
+  test "Tree_cursor.start_position" Position.(row start_pos = 0 && column start_pos = 0);
+  let end_pos = Tree_cursor.end_position cursor in
+  test "Tree_cursor.end_position" Position.(row end_pos = 0 && column end_pos = 9);
+  test "Tree_cursor.start_index" (Tree_cursor.start_index cursor = 0);
+  test "Tree_cursor.end_index" (Tree_cursor.end_index cursor = 9);
   Console.log
     Tree_cursor.
-      [ node_type cursor
-      ; node_type_id cursor
+      [ node_type_id cursor
       ; node_id cursor
       ; node_is_named cursor
       ; node_is_missing cursor
-      ; node_text cursor
-      ; start_position cursor
-      ; end_position cursor
-      ; start_index cursor
-      ; end_index cursor
       ; current_field_id cursor
         (* TODO: broken ; current_field_name cursor *)
       ]
